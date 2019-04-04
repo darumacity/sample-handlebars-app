76f43ee799587e14c619b45bf22a7af5102b3713
磯崎 正貴 <masataka-isozaki@gmo.jp>
2019-04-04 22:22:23 +0900

    Connect to database

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'blog_user',
+  password: 'password',
+  database: 'blog',
+})
+
 router.get('/', function (req, res, next) {
-  res.render('entries/index', {
-    entries: [
-      {
-        id: 1,
-        title: 'Coffee Pic',
-        description: 'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consecteturad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.',
-        image: 'coffee.jpg',
-      },
-      {
-        id: 2,
-        title: 'On the road again',
-        description: 'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consecteturad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.',
-        image: 'road.jpg',
-      },
-      {
-        id: 3,
-        title: 'I couldn’t take any pictures but this was an amazing thing…',
-        description: 'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consecteturad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.',
-      },
-      {
-        id: 4,
-        title: 'Shopping',
-        description: 'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consecteturad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.',
-        image: 'shopping.jpg',
-      },
-    ]
+  connection.query('select EntryId as id, Title as title, Description as description, Image as image from entries', function (error, results, fields) {
+    res.render('entries/index', { entries: results });
   });
 });
 
 router.get('/entries/:id', function (req, res, next) {
-  console.log(req.params.id);
-  res.render('entries/detail', {
-    title: 'On the road again',
-    content: 'test',
+  connection.query('select Title as title, Content as content from entries where EntryId = ?', [req.params.id], function (error, results, fields) {
+
+    if (results.length === 0) {
+      res.send(404);
+      return;
+    }
+
+    res.render('entries/detail', results[0]);
   });
 });
 
