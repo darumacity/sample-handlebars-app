05c20fed55f4985a4b4c15fa631e2136ee82bd72
磯崎 正貴 <masataka-isozaki@gmo.jp>
2019-04-05 21:46:43 +0900

    内容をマークダウンで表示するよう対応

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,26 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: {
-        vendor: ['marked'],
+        vendor: ['marked', 'jquery'],
     },
     output: {
         path: __dirname + '/public/javascripts',
         filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [{
+            test: require.resolve('jquery'),
+            use: [{
+                loader: 'expose-loader',
+                options: '$',
+            }]
+        },
+        {
+            test: require.resolve('marked'),
+            use: [{
+                loader: 'expose-loader',
+                options: 'marked',
+            }]
+        }]
     },
 };
